c827d4a0dd2e7f415f64f904d329fa04fcffa96e
clylau <cllau@g.hmc.edu>
2021-02-10 18:34:25 -0800

    Point tracking main function

--- Lab2/e206_lab_00.py
+++ Lab2/e206_lab_00.py
@@ -42,10 +42,58 @@ def main():
   
   env.close()
   
+def main2():
+  # Create a motion planning problem and solve it
+  current_state, desired_state, objects, walls = create_motion_planning_problem()
+  desired_traj = desired_state
+  
+  # Construct an environment
+  env = gym.make("fetch-v0") # <-- this we need to create
+  env.set_parameters(TIME_STEP_SIZE, [])
+  env.render('human')
+  env.reset()
+
+  # Create the trajectory and tracking controller
+  controller = PointTracker()
+  traj_tracker = TrajectoryTracker(desired_traj)
+
+  keep_tracking = True
+  index = 0
+  len_traj = len(desired_traj)
+  
+  # Create the feedback loop
+  time.sleep(1)
+  current_time_stamp = 0
+  observation = [0,0,0,0,0]
+  actual_traj = []
+
+  while keep_tracking:
+    current_state = [current_time_stamp, observation[0], observation[1], observation[2]]
+
+    desired_state = desired_traj[index]
+    print(desired_state)
+
+    action = controller.point_tracking_control(desired_state, current_state)
+    observation, reward, done, dummy = env.step(action)
+    env.render('human')
+    actual_traj.append(current_state)
+    current_time_stamp += TIME_STEP_SIZE
+
+    if controller.traj_tracked:
+      index = index + 1
+      controller.traj_tracked = False
+
+      if index == len_traj:
+        keep_tracking = False
+
+  time.sleep(2)
+  plot_traj(desired_traj, actual_traj, [], [])
+  env.close()
+
 def create_motion_planning_problem():
   current_state = [0, 0, 0, 0]
-  desired_state = [0, 0, -2, -np.pi/2] #[2, 2, 0, 0] #current_state #[20, 2.5, -2.5, 1.57]
-  #desired_state = [[0, 2, 0, 0], [0, 0, 0, 0], [0, 2, 2, 0], [0, 0, 0, 0], [0, 0, 2, np.pi/2], [0, 0, 0, 0], [0, -2, 2, 0], [0, 0, 0, 0], [0, -2, 0, 0], [0, 0, 0, 0], [0, -2, -2, 0], [0, 0, 0, 0], [0, 0, -2, -np.pi/2], [0, 0, 0, 0], [0, 2, -2, 0], [0, 0, 0, 0],]
+  #desired_state = [0, -2, 0, 0] #[2, 2, 0, 0] #current_state #[20, 2.5, -2.5, 1.57]
+  desired_state = [[0, 2, 0, 0], [0, 0, 0, 0], [0, 2, 2, 0], [0, 0, 0, 0], [0, 0, 2, np.pi/2], [0, 0, 0, 0], [0, -2, 2, 0], [0, 0, 0, 0], [0, -2, 0, 0], [0, 0, 0, 0], [0, -2, -2, 0], [0, 0, 0, 0], [0, 0, -2, -np.pi/2], [0, 0, 0, 0], [0, 2, -2, 0], [0, 0, 0, 0],]
   maxR = 8
   walls = [[-maxR, maxR, maxR, maxR, 2*maxR], [maxR, maxR, maxR, -maxR, 2*maxR], [maxR, -maxR, -maxR, -maxR, 2*maxR], [-maxR, -maxR, -maxR, maxR, 2*maxR] ]
   objects = [[4, 0, 1.0], [-2, -3, 1.5]]
@@ -53,6 +101,7 @@ def create_motion_planning_problem():
   return current_state, desired_state, objects, walls
 
 if __name__ == '__main__':
-    main()
+    #main()
+    main2()
     
     
